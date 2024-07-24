969c982311244e7550a06443c110e6542dc9ac21
Aytac <aramazanli@std.beu.edu.az>
2024-07-24 13:48:30 +0400

    added ActivityResponse

---
+++ src/main/java/com/example/discovery_country/model/dto/response/ActivityResponse.java
@@ -0,0 +1,61 @@
+package com.example.discovery_country.model.dto.response;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ActivityResponse {
+
+
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    double price;
+
+    String mapUrl;
+    
+    @Column(nullable = false)
+    double latitude;
+    
+    @Column(nullable = false)
+    double longitude;
+    
+    @Column(columnDefinition = "TEXT")
+    String description;
+
+    @Column(nullable = false)
+    LocalDateTime startDate;
+
+    @Column(nullable = false)
+    LocalDateTime endDate;
+
+    @Column(nullable = false)
+    LocalDateTime registrationDeadline;
+
+    @Column(nullable = false)
+    String contact;
+
+    @Column(columnDefinition = "TEXT")
+    String requirements;
+
+    @Column(nullable = false)
+    Integer numberOfPeople;
+
+    List<Long> imageIds;
+    Long activityCategoryId;
+
+}
+
+
+
