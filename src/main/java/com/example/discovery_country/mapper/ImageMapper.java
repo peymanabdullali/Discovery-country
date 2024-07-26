97705e5a4d367340f4cd213a8fc169f207d9510d
Aytac <aramazanli@std.beu.edu.az>
2024-07-26 20:27:29 +0400

    added related with Image

--- src/main/java/com/example/discovery_country/mapper/ImageMapper.java
+++ src/main/java/com/example/discovery_country/mapper/ImageMapper.java
@@ -4,16 +4,15 @@ import com.example.discovery_country.dao.entity.ImageEntity;
 import com.example.discovery_country.model.dto.request.ImageRequest;
 import com.example.discovery_country.model.dto.response.ImageResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.factory.Mappers;
-
-import java.util.List;
-
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ImageMapper {
 
     ImageEntity mapToEntity(ImageRequest request);
 
-    ImageResponse mapToResponse(ImageEntity entity);
+    ImageResponse mapToResponse(ImageEntity imageEntity);
+
+    void mapForUpdate(@MappingTarget ImageEntity imageEntity,ImageRequest imageRequest);
 
 }
