8ef350ad72f6726ee6ce376ee603b8bbdfab9bbe
Aytac <aramazanli@std.beu.edu.az>
2024-07-30 17:52:40 +0400

    ActivityCategoryMapper changed

--- src/main/java/com/example/discovery_country/mapper/ActivityCategoryMapper.java
+++ src/main/java/com/example/discovery_country/mapper/ActivityCategoryMapper.java
@@ -1,22 +1,31 @@
 package com.example.discovery_country.mapper;
 
 import com.example.discovery_country.dao.entity.ActivityCategoryEntity;
+import com.example.discovery_country.dao.entity.ImageEntity;
+import com.example.discovery_country.dao.entity.RegionEntity;
 import com.example.discovery_country.model.dto.request.ActivityCategoryRequest;
 import com.example.discovery_country.model.dto.response.ActivityCategoryResponse;
+import com.example.discovery_country.model.dto.response.ImageResponse;
+import com.example.discovery_country.model.dto.response.RegionResponseForActivityCategory;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 
 @Mapper
 public interface ActivityCategoryMapper {
-    ActivityCategoryMapper INSTANCE = Mappers.getMapper(ActivityCategoryMapper.class);
+   ActivityCategoryMapper INSTANCE = Mappers.getMapper(ActivityCategoryMapper.class);
 
 
     ActivityCategoryEntity mapToEntity(ActivityCategoryRequest activityCategoryRequest);
 
     ActivityCategoryResponse mapToResponse(ActivityCategoryEntity activityCategoryEntity);
+
+    List<RegionResponseForActivityCategory> mapToRegionResponseForActivityCategoryList(List<RegionEntity> entities);
+
 
     void mapForUpdate (@MappingTarget ActivityCategoryEntity entity, ActivityCategoryRequest request);
 }
