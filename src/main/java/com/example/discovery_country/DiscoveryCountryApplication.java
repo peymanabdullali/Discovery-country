18e1dc81e9c1f5d6c38b918547ceea6904bcc179
pabdu <abdullalipeyman@gmail.com>
2024-08-05 11:56:06 +0400

    some changes

--- src/main/java/com/example/discovery_country/DiscoveryCountryApplication.java
+++ src/main/java/com/example/discovery_country/DiscoveryCountryApplication.java
@@ -2,8 +2,11 @@ package com.example.discovery_country;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+
 public class DiscoveryCountryApplication {
 
     public static void main(String[] args) {
