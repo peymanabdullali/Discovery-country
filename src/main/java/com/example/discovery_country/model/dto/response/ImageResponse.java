68a6505365993c95ea984e4aed34d7fdf623a2ec
pabdu <abdullalipeyman@gmail.com>
2024-08-06 23:21:26 +0400

    change ImageResponse

--- src/main/java/com/example/discovery_country/model/dto/response/ImageResponse.java
+++ src/main/java/com/example/discovery_country/model/dto/response/ImageResponse.java
@@ -1,24 +1,25 @@
 package com.example.discovery_country.model.dto.response;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
-@Builder
 @NoArgsConstructor
 public class ImageResponse {
 
-    long id;
-    String name;
-    String url;
 
+    String text;
+    Long id;
 //    HotelRoomsResponse hotelRoom;
 //    HomeHotelResponse homeHotel;
 //    ActivityResponse activity;
 //    ScenicSpotResponse scenicSpot;
-//    RestaurantResponse restaurant;
+//    RestaurantResponse restaurant;
 }
