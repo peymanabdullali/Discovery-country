53f7f070448d4b0af5b7fe631bacabbbe460cc4b
pabdu <abdullalipeyman@gmail.com>
2024-08-17 23:03:26 +0400

    fixing ZoneSpecification

--- src/main/java/com/example/discovery_country/service/specification/ZoneSpecification.java
+++ src/main/java/com/example/discovery_country/service/specification/ZoneSpecification.java
@@ -1,6 +1,5 @@
 package com.example.discovery_country.service.specification;
 
-import com.example.discovery_country.dao.entity.ActivityCategoryEntity;
 import com.example.discovery_country.dao.entity.ZoneEntity;
 import com.example.discovery_country.model.dto.criteria.CriteriaRequestForName;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -14,12 +13,15 @@ import java.util.List;
 
 public class ZoneSpecification implements Specification<ZoneEntity> {
 
-    public static Specification<ZoneEntity>getZoneByCriteria(CriteriaRequestForName criteriaRequest){
-        return (root, query, criteriaBuilder) ->{
-            List<Predicate> predicates=new ArrayList<>();
+    public static Specification<ZoneEntity> getZoneByCriteria(CriteriaRequestForName criteriaRequest) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
 
-            if(criteriaRequest.getName()!=null && criteriaRequest.getName().isEmpty()){
-                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")),"%" + criteriaRequest));
+            if (criteriaRequest.getName() != null && !criteriaRequest.getName().isEmpty()) {
+                predicates.add(criteriaBuilder.like(
+                        criteriaBuilder.lower(root.get("name")),
+                        "%" + criteriaRequest.getName().toLowerCase() + "%"
+                ));
             }
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
