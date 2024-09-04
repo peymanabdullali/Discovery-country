fbefdb68bfbe10889065702ad3ba0875c06ab32e
Aytac <aramazanli@std.beu.edu.az>
2024-09-04 22:21:56 +0400

    GlobalExceptionHandler added FileSizeExceededException

---
+++ src/main/java/com/example/discovery_country/exception/FileSizeExceededException.java
@@ -0,0 +1,8 @@
+package com.example.discovery_country.exception;
+
+public class FileSizeExceededException extends RuntimeException {
+    public FileSizeExceededException(String message) {
+        super(message);
+
+    }
+}
--- src/main/java/com/example/discovery_country/exception/errorHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/discovery_country/exception/errorHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.discovery_country.exception.errorHandler;
 
+import com.example.discovery_country.exception.FileSizeExceededException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,5 +28,22 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        return ResponseEntity
+                .status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body("Fayl  limitini aşır");
+    }
+
+    @ExceptionHandler(FileSizeExceededException.class)
+    public ResponseEntity<String> handleFileSizeExceededException(FileSizeExceededException ex) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(ex.getMessage());
     }
 }
