cc8bf4da367d1592e07e452bb24234524800258b
pabdu <abdullalipeyman@gmail.com>
2025-02-07 19:15:40 +0400

    update scheduler

--- src/main/java/com/example/discovery_country/service/scheduler/ReservationScheduler.java
+++ src/main/java/com/example/discovery_country/service/scheduler/ReservationScheduler.java
@@ -5,6 +5,7 @@ import com.example.discovery_country.dao.repository.RoomReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -13,6 +14,7 @@ public class ReservationScheduler {
      private final RoomReservationRepository reservationRepository;
 //    @Scheduled(cron = "0 0 12,0 * * ?")
 @Scheduled(cron = "*/10 * * * * *")
+@Transactional
 public void updateRoomAvailability(){
     System.out.println("salam");
     roomRepository.updateAvailableRooms();
